d94fc2d912ddf5611daa4eb0c0e653e529141373
BHaskarMishra05 <bhaskarmishra1590@gmail.com>
2025-11-11 13:07:23 +0530

    level 6,7

--- app.py
+++ app.py
@@ -22,5 +22,22 @@ def level_2():
 def level_3():
     return render_template('level-3.html')
 
+
+@app.route('/level-4')
+def level_4():
+    return render_template('level-4.html')
+
+@app.route('/level-5')
+def level_5():
+    return render_template('level-5.html')
+
+@app.route('/level-6')
+def level_6():
+    return render_template('level-6.html', level=6)
+
+@app.route('/level-7')
+def level_7():
+    return render_template('level-7.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
